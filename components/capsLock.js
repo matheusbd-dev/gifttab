4830b77c7fec20fcf8995be9b36ac67271344e3d
matheusbd-dev <matheusbezerradomingos@gmail.com>
2026-07-10 10:58:59 +0000

    Nova Branch

--- components/capsLock.js
+++
@@ -1,9 +0,0 @@
-<CapsLock texto="Me deixe em CapsLock por favor" />;
-
-function CapsLock(props) {
-  const textoInserido = props.texto;
-  const textoEmCapsLock = textoInserido.toUpperCase();
-  return <div>{textoEmCapsLock}</div>;
-}
-
-export default CapsLock;
