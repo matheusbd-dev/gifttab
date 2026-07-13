88dd176f380bb02a4627c1265ec73044fe93e66c
Matheus Bezerra Domingos <168386167+matheusb-dev@users.noreply.github.com>
2026-07-13 15:34:04 +0000

    BACKUP

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-write">gifttab</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
